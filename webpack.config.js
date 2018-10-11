ad6611e1bb2a2e714274eec475ffcb2d6b56fd23
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-11 18:20:36 +1300

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: path.join(__dirname, 'server/public'),
+    path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
     extensions: ['.js', '.jsx']
   },
   devtool: 'source-map'
-}
+};
