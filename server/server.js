36d93fa5c57a8b39352b14c30c14ee5875d5791d
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-11 18:12:58 +1300

    created post route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const express = require('express');
 
-const server = express()
+const server = express();
 
-server.use(express.json())
-server.use(express.static(path.join(__dirname, './public')))
+server.use(express.json());
+server.use(express.static(path.join(__dirname, './public')));
 
-module.exports = server
+server.get('/hi', (req, res) => {
+  res.send('hi');
+});
+
+server.post('/', (req, res) => {
+  console.log(req);
+  res.json('hello');
+});
+
+module.exports = server;
