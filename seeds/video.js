a11742b03b67dd50c0538c7e987bd7e721091ce0
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-14 11:28:42 +1300

    punc

--- seeds/video.js
+++ seeds/video.js
@@ -5,17 +5,17 @@ exports.seed = function(knex, Promise) {
       return knex('videos').insert([
         {
           id: 1,
-          snippet: 'Wake up and win the day.',
+          snippet: 'Wake up and win the day',
           video: 'https://www.youtube.com/watch?v=1sgLxp6r2OE'
         },
         {
           id: 2,
-          snippet: 'How successful people think.',
+          snippet: 'How successful people think',
           video: 'https://www.youtube.com/watch?v=zCyB2DQFdA0'
         },
         {
           id: 3,
-          snippet: 'Believe in yourself.',
+          snippet: 'Believe in yourself',
           video: 'https://www.youtube.com/watch?v=AjZ0KbJcav0'
         }
       ]);
