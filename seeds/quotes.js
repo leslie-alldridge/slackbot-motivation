97d7312114be5b38faafde34ccb4d826f83fbbeb
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-10-19 07:47:07 +1300

    updating seeds
    updating seeds

--- seeds/quotes.js
+++ seeds/quotes.js
@@ -30,6 +30,46 @@ exports.seed = function(knex, Promise) {
           id: 7,
           quote:
             'Failure will never overtake me if my determination to succeed is strong enough.'
+        },
+        {
+          id: 8,
+          quote:
+            'Stay happy, stay humble!'
+        },
+        {
+          id: 9,
+          quote:
+            'The seeds of destruction lie in the roots of comparison.'
+        },
+        {
+          id: 10,
+          quote:
+            'Define your normal.'
+        },
+        {
+          id: 11,
+          quote:
+            'What is uncomfortable now, will become comfortable later.'
+        },
+        {
+          id: 12,
+          quote:
+            'Success isn’t found at the destination, but rather in the journey.'
+        },
+        {
+          id: 13,
+          quote:
+            'Great things have never come from the comfort zone.'
+        },
+        {
+          id: 14,
+          quote:
+            'The past doesn’t determine the future.'
+        },
+        {
+          id: 15,
+          quote:
+            'Every day may not be good, but there is good in every day.'
         }
       ]);
     });
